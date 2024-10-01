ccbca80e29f5bf5b56d329e75b0f29d45282faa2
Nifemi Bosun <144700801+nifemibosun@users.noreply.github.com>
2024-10-01 13:13:18 +0100

    Update crypto.ts

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,21 +1,24 @@
 import crypto from 'crypto';
 
+export function hashFunc(data: Buffer): Buffer {
+    const hashedData = crypto.createHash('sha256').update(
+        crypto.createHash('sha256').update(data).digest()
+    ).digest();
+    return hashedData;
+}
+
 export function hashBlock(data: string): string {
     if (typeof data !== 'string') {
         throw new TypeError('Data must be a string.');
     }
-
     const hashedData = crypto.createHash('sha256').update(
         crypto.createHash('sha256').update(data).digest('hex')
     ).digest('hex');
-    
     return hashedData;
 }
 
-export function hashTransaction(amount: number, sender: string, recipient: string): string {
-    const transactionDataAsString = `${amount}${sender}${recipient}`;
-    
+export function hashTransaction<T>(transaction: T): string {
+    const transactionDataAsString = `${transaction.amount}${transaction.sender}${transaction.recipient}`;
     const hashedTransaction = hashBlock(transactionDataAsString);
-
     return hashedTransaction;
 }
