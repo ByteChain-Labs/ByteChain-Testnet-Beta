cc2f7be5f7000e51d104496f909c8fcaa18bc29d
Nifemi Bosun <144700801+nifemibosun@users.noreply.github.com>
2024-10-01 09:46:27 +0100

    Update pow.ts

--- src/consensus/pow.ts
+++ src/consensus/pow.ts
@@ -1,1 +1,11 @@
-
+function ProofOfWork(blockhash: string): number { 
+  let hash: string;
+  while (true) {
+    hash 
+    if (blockhash.substring(0, MiningDifficulty) === '0'.repeat(MiningDifficulty)) {
+      break;
+    }
+    this.nonce++;
+  };
+  return this.nonce;
+} 
